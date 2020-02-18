a1682303fe62e60b5891f8147a4de83cac737e1b
Kyryl Krylov, CPA <kirill.krylov@gmail.com>
2020-02-18 19:09:46 +0200

    Add project files.

---
+++ Files/cs/EL/ContactEl.cs
@@ -0,0 +1,26 @@
+﻿using global::Common.Logging;
+using Terrasoft.Core;
+using Terrasoft.Core.Entities;
+using Terrasoft.Core.Entities.Events;
+
+namespace GuidedLearningClio.Files.cs.el
+{
+    /// <summary>
+    /// Listener for 'EntityName' entity events.
+    /// </summary>
+    /// <seealso cref="Terrasoft.Core.Entities.Events.BaseEntityEventListener" />
+    [EntityEventListener(SchemaName = "Contact")]
+    class ContactEventListener : BaseEntityEventListener
+    {
+        private static readonly ILog _log = LogManager.GetLogger("DevTrainingLogger");
+        public override void OnSaved(object sender, EntityAfterEventArgs e)
+        {
+            base.OnSaved(sender, e);
+            Entity entity = (Entity)sender;
+            //UserConnection userConnection = entity.UserConnection;
+
+            string message = $"Changing name for {entity.GetTypedColumnValue<string>("Name")}";
+            _log.Info(message);
+        }
+    }
+}
